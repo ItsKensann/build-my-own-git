b4bba737554dd76251c9e3ab5eaa231d7aaa012b
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-02-24 01:54:36 -0800

    implemented cat-file ad hash-object

--- libwyag.py
+++ libwyag.py
@@ -161,6 +161,16 @@ class GitObject (object):
     def init(self):
         pass
 
+class GitBlob(GitObject):
+    "Subclass of GitObject"
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
 def object_read(repo, sha):
     """Reads object SHA from the git repo.
        00000000  63 6f 6d 6d 69 74 20 31  30 38 36 00 74 72 65 65  |commit 1086.tree|
@@ -200,6 +210,7 @@ def object_read(repo, sha):
         return c(raw[y+1:])
 
 def object_write(obj, repo=None):
+    """Takes in an object, writes it in the proper directory based on its SHA"""
     # Serialize object data
     data = obj.serialize()
     # Add header, make everything in byte format
@@ -218,30 +229,69 @@ def object_write(obj, repo=None):
 
     return sha
 
-class GitBlob(GitObject):
-    "Subclass of GitObject"
-    fmt = b"blob"
+def object_find(repo, name, fmt=None, follow=True):
+    # TODO Will implement full function later, since there areother ways to refer to objects
+    return name
 
-    def serialize(self):
-        return self.blobdata
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
 
-    def deserialize(self, data):
-        self.blobdata = data
+def hash_object(fd, fmt, repo=None):
+    """Hash object, writing it to .git/objects if provided"""
+    data = fd.read()
+
+    # Choose constructor according to fmt argument
+    match fmt:
+        case b"commit" : obj=GitCommit(data)
+        case b"tree" : obj=GitTree(data)
+        case b"tag" : obj=GitTag(data)
+        case b"blob" : obj=GitBlob(data)
+
+    return object_write(obj, repo)
+
+# -- 
 
 # Bridge functions
 def cmd_init(args):
     repo_create(args.path)
 
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find() # Look for git repo if -w command provided
+    else:
+        repo = None
+
+    # open contents of file and hash object
+    with open(args.path, "rb") as fd:
+        sha = hash_object(fd, args.type.encode(), repo)
+        print(sha)
+
 def main(argv=sys.argv[1:]):
     argparser = argparse.ArgumentParser(description="Program")
-    # Initialize subparsers
+    # Initialize subparsers, which are the subcommands
     argsubparsers = argparser.add_subparsers(title="Commands", dest="command") # Tells python to save the word after the program name
     argsubparsers.required = True # Ensures user must provid a command
 
-    # Add the inidividual subcommands
+    # -- Add the inidividual subcommands
     # Git init
     argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository")
     argsp.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository.")
+
+    # cat-file
+    argsp = argsubparsers.add_parser("cat-file", help="Provide content of repository objects")
+    argsp.add_argument("type", metavar="type", choices=["blob", "commit", "tag", "tree"], help="Specify the type")
+    argsp.add_argument("object", metavar="object", help="The object to display")
+
+    # hash-object
+    argsp = argsubparsers.add_parser("hash-object", help="Compute object ID and optionally creates a blob from a file")
+    argsp.add_argument("-t", metavar="type", dest="type", choices=["blob", "commit", "tag", "tree"], default="blob", help="Specify the type")
+    argsp.add_argument("-w", dest="write", action="store_true", help="Actually write the object into the database")
+    argsp.add_argument("path", help="Read object from <file>")
 
     args = argparser.parse_args(argv)
 
